97e8e0dd401604eeb8ef39fe957b43f278cd103b
Henry Garner <henry@henrygarner.com>
2012-09-30 13:03:36 +0100

    Update project description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-box "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A plugin for managing a project-specific virtual machine."
+  :url "https://github.com/henrygarner/lein-box"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
