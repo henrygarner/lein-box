519ce0e0e5e4a28479bbe24c6c3c9c8b165e7030
Henry Garner <henry@henrygarner.com>
2012-11-01 13:13:26 +0000

    Functions to import a VM

--- src/leiningen/box/providers/virtualbox.clj
+++ src/leiningen/box/providers/virtualbox.clj
@@ -13,8 +13,8 @@
        (into {})))
 
 (defn vm-info
-  ([] (:out (sh "VBoxManage" "showvminfo" (store/active-default) "--machinereadable")))
-  ([key] (-> (vm-info) info->map (get key))))
+  ([] (info->map (:out (sh "VBoxManage" "showvminfo" (store/active-default) "--machinereadable"))))
+  ([key] (-> (vm-info) (get key))))
 
 (defn vm-start []
   (let [result (sh "VBoxManage" "startvm" (store/active-default) "--type" "headless")]
@@ -25,3 +25,40 @@
   (-> (sh "VBoxManage" "controlvm" (store/active-default) "savestate")
       :exit
       (= 0)))
+
+(defn vm-destroy []
+  (sh "VBoxManage" "unregistervm" (store/active-default) "--delete"))
+
+(defn vm-directory []
+  ; TODO: dynamically determine this directory
+  "/Users/henry/.vagrant.d/boxes/")
+
+(defn vm-import [name]
+  ; TODO: return the machine id so that can be named
+  ; Suggested VM name (.+?)
+  ; name {(.+?)}
+  (sh "VBoxManage" "import" (str (vm-directory) name "/box.ovf")))
+
+(defn vm-forwarded-ports []
+  (let [forwarding (filter #(.startsWith (first %) "Forwarding") (vm-info))]
+    (map #(map (s/split (last %) #",") [0 3 5]) forwarding)))
+
+(defn vm-clear-forwarded-ports []
+  (let [port-names (map first (vm-forwarded-ports))
+        ; TODO: figure out how to reliably determine nat
+        args (mapcat #(["--natpf1" "delete" %]) port-names)]
+    (apply sh "VBoxManage" "modifyvm" (store/active-default) args)))
+
+(defn vm-set-name [name]
+  (sh "VBoxManager" "modifyvm" (store/active-default) "--name" name))
+
+(defn forward-ports [ports]
+  (comment "VBoxManage modifyvm uuid --natpf1", "ssh,tcp,,2222,,22"))
+
+(defn check-accessible []
+  (not= (:state (vm-info))) "inaccessible")
+
+(defn clean-machine-folder []
+  (comment "VirtualBox bug means .xml-prev files may be left over"))
+
+(defn clear-forwarded-ports [])
